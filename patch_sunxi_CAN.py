8c2638c485a742b54f0282e251ed7f6f9d4e05c1
Patrick Menschel <menschel.p@posteo.de>
2017-07-22 13:29:17 +0200

    added check for file patching

--- patch_sunxi_CAN.py
+++ patch_sunxi_CAN.py
@@ -3,15 +3,23 @@
 # purpose: as SUNXI kernel repositories do not feature device tree overlays, 
 #          this is a quick hack to activate CAN for the board
 #          just call it in the make file before compiling dts
-
+import os
 
 def patch(fpath):
-    with open(fpath,"a") as f:
-        f.write("""&can0 {
+    needpatch = False
+    with open(fpath) as f:
+        needpatch = not ("&can0" in f.read())
+    if needpatch:
+        print("patching {0}".format(os.path.split(fpath)[-1]))
+        with open(fpath,"a") as f:
+            f.write("""&can0 {
 	pinctrl-names = "default";
 	pinctrl-0 = <&can0_pins_a>;
 	status = "okay";
 };""")
+        print("patched {0}".format(os.path.split(fpath)[-1]))
+    else:
+        print("no patch required")
 
 
 if __name__ == "__main__":
